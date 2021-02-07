98a33113a69bb75f1764d8a1ff902f12d0a03010
Sam Jacobson <34726399+samja123@users.noreply.github.com>
2021-02-07 10:13:51 -0500

    Update sj3512_hw5_q2.cpp

--- sj3512_hw5_q2.cpp
+++ sj3512_hw5_q2.cpp
@@ -14,116 +14,36 @@ int main(){
     lowerLimit = 1;
     upperLimit = 100;
     guessesLeft = 5;
-    guessesUsed = 1;
     srand(time(0));
     randomNumber = (rand() % 100) + 1;
+    cout<<randomNumber<<endl;
     cout<<"Range: ["<<lowerLimit<<", "<<upperLimit<<"], Number of guess left: "<<guessesLeft<<endl;
     cout<<"Your guess: ";
     cin>>guess;
-    guessedCorrectNumber = false;
-    while(guessedCorrectNumber == false){
-        while(guessesLeft == 5){
-            if(guess == randomNumber){
-                cout<<"Congrats! You guessed my number in "<<guessesUsed<<" guesses."<<endl;
-                guessedCorrectNumber = true;
-                break;
+        for(guessesUsed = 1; guessesUsed <= 5; guessesUsed++) {
+            guessesLeft = 5 - guessesUsed;
+            if (guess == randomNumber) {
+                cout << "Congrats! You guessed my number in " << guessesUsed << " guesses." << endl;
+                guessesUsed =  6;
             }
-            else if(guess < randomNumber){
-                lowerLimit = guess + 1;
-                cout<<"Wrong! My number is bigger."<<endl;
-                guessesLeft--;
-                guessesUsed++;
-            }
-            else {
-                upperLimit = guess - 1;
-                cout<<"Wrong! My number is smaller."<<endl;
-                guessesLeft--;
-                guessesUsed++;
-            }
-        }
-        while(guessesLeft == 4){
-            cout<<"Range: ["<<lowerLimit<<", "<<upperLimit<<"], Number of guesses left: "<<guessesLeft<<endl;
-            cout<<"Your guess: ";
-            cin>>guess;
-            if(guess == randomNumber){
-                cout<<"Congrats! You guessed my number in "<<guessesUsed<<" guesses."<<endl;
-                guessedCorrectNumber = true;
-                break;
-            }
-            else if(guess < randomNumber){
-                lowerLimit = guess + 1;
-                cout<<"Wrong! My number is bigger."<<endl;
-                guessesLeft--;
-                guessesUsed++;
-            }
-            else {
-                upperLimit = guess - 1;
-                cout<<"Wrong! My number is smaller."<<endl;
-                guessesLeft--;
-                guessesUsed++;
-            }
-        }
-        while(guessesLeft == 3){
-            cout<<"Range: ["<<lowerLimit<<", "<<upperLimit<<"], Number of guesses left: "<<guessesLeft<<endl;
-            cout<<"Your guess: ";
-            cin>>guess;
-            if(guess == randomNumber){
-                cout<<"Congrats! You guessed my number in "<<guessesUsed<<" guesses."<<endl;
-                guessedCorrectNumber = true;
-                break;
-            }
-            else if(guess < randomNumber){
-                lowerLimit = guess + 1;
-                cout<<"Wrong! My number is bigger."<<endl;
-                guessesLeft--;
-                guessesUsed++;
-            }
-            else {
-                upperLimit = guess - 1;
-                cout<<"Wrong! My number is smaller."<<endl;
-                guessesLeft--;
-                guessesUsed++;
-            }
-        }
-        while(guessesLeft == 2){
-            cout<<"Range: ["<<lowerLimit<<", "<<upperLimit<<"], Number of guesses left: "<<guessesLeft<<endl;
-            cout<<"Your guess: ";
-            cin>>guess;
-            if(guess == randomNumber){
-                cout<<"Congrats! You guessed my number in "<<guessesUsed<<" guesses."<<endl;
-                guessedCorrectNumber = true;
-                break;
-            }
-            else if(guess < randomNumber){
-                lowerLimit = guess + 1;
-                cout<<"Wrong! My number is bigger."<<endl;
-                guessesLeft--;
-                guessesUsed++;
-            }
-            else {
-                upperLimit = guess - 1;
-                cout<<"Wrong! My number is smaller."<<endl;
-                guessesLeft--;
-                guessesUsed++;
-            }
-        }
-        while(guessesLeft == 1) {
-            cout << "Range: [" << lowerLimit << ", " << upperLimit << "], Number of guesses left: " << guessesLeft << endl;
-            cout << "Your guess: ";
-            cin >> guess;
-            if(guess == randomNumber){
-                cout<<"Congrats! You guessed my number in "<<guessesUsed<<" guesses."<<endl;
-                guessedCorrectNumber = true;
-                guessesLeft--;
-            }
-            else if (guess != randomNumber) {
+            else if(guessesLeft == 0){
                 cout << "Out of guesses! My number is: " << randomNumber << endl;
-                guessedCorrectNumber = true;
-                guessesLeft--;
             }
-
+            else if (guess < randomNumber) {
+                lowerLimit = guess + 1;
+                cout << "Wrong! My number is bigger." << endl;
+                cout << "Range: [" << lowerLimit << ", " << upperLimit << "], Number of guesses left: " << guessesLeft
+                     << endl;
+                cout << "Your guess: ";
+                cin >> guess;
+            } else {
+                upperLimit = guess - 1;
+                cout << "Wrong! My number is smaller." << endl;
+                cout << "Range: [" << lowerLimit << ", " << upperLimit << "], Number of guesses left: " << guessesLeft
+                     << endl;
+                cout << "Your guess: ";
+                cin >> guess;
+            }
         }
-
-    }
     return 0;
 }
